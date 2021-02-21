06a17e432d340d900f493cd887eb273297d52a13
Morrism1 <morrismontana0@gmail.com>
2021-02-22 01:38:43 +0200

    Add search functionality to the project from openweathermap api

--- src/modules/search.js
+++ src/modules/search.js
@@ -1,3 +1,6 @@
+import todayWeather from './todayWeater';
+import { setForecastWeather } from './forecast';
+
 const searchForm = document.querySelector('[data-search-form]');
 const searchInput = document.querySelector('[data-search-input]');
 const dataLocation = document.querySelector('[data-weather-location]');
@@ -6,22 +9,56 @@ const API_KEY = 'c09966794fa0f3ef1e2d835c3c916ccf';
 
 let address = '';
 
+const removeQuotes = (str) => {
+  return str.replace(/^"|"$/g, '');
+};
+address = removeQuotes('kigali');
+const api = removeQuotes(API_KEY);
+
 const initializeSearch = () => {
   searchEvents();
 };
 
 const searchEvents = () => {
-  searchForm.addEventListener('submit', (e) => {
+  searchForm.addEventListener('submit', async (e) => {
     e.preventDefault();
     if (searchInput.value === '') return;
     address = searchInput.value;
     searchInput.value = '';
+    let str = removeQuotes(address);
+    console.log(str);
+    const { lat, lng } = await getCoordinates(str);
+    console.log(lat, lng);
+    getWeatherData(lat, lng);
     render();
   });
 };
 
+const getCoordinates = async (address) => {
+  const URL = `https://api.openweathermap.org/geo/1.0/direct?q=${address}&limit=5&appid=${api}`;
+  const getData = await fetch(URL);
+  const parsedData = await getData.json();
+  const latLong = {
+    lat: parsedData[0].lat,
+    lng: parsedData[0].lon,
+  };
+  console.log(latLong);
+
+  return latLong;
+};
+
+const getWeatherData = async (lat, lng) => {
+  const URL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lng}&appid=${api}`;
+  const data = await fetch(URL);
+  const parsedData = await data.json();
+  const weather = parsedData.current;
+  const dailyForecast = parsedData.daily;
+  todayWeather(weather);
+  setForecastWeather(dailyForecast);
+};
+
 const render = () => {
-  dataLocation.textContent = address;
+  dataLocation.textContent = address.toUpperCase();
 };
 
 export default initializeSearch;
